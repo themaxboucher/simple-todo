c2df8ff6d555bfd0bfec599fe8d735798b070215
Maxime Boucher <maxime@maximeboucher.com>
2022-08-17 16:19:39 -0600

    feat: Work on drag and drop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import './App.css';
-import NewTask from './NewTask';
-import TaskList from './TaskList';
-import Header from './Header';
+import "./App.css";
+import NewTask from "./NewTask";
+import TaskList from "./TaskList";
+import Header from "./Header";
 import { useState } from "react";
 import { DndProvider } from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
-
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   const [newTask, setNewTask] = useState({});
   const handleChange = ({ target }) => {
     const { name, value } = target;
-    setNewTask((prev) => ({ 
-      ...prev, 
-      id: Date.now(), 
-      [name]: value, 
+    setNewTask((prev) => ({
+      ...prev,
+      id: Date.now(),
+      [name]: value,
       done: false,
-      index: 1
+      index: 1,
     }));
   };
 
-  const [taskList, setTaskList] = useState([{title: 'Go add some tasks to this list', id: Date.now(), done: false}]);
+  const [taskList, setTaskList] = useState([
+    { title: "Add some tasks to this list", id: Date.now(), done: false },
+  ]);
   const handleSubmit = (event) => {
     // If no task title do nothing
     event.preventDefault();
@@ -32,47 +33,48 @@ function App() {
   };
   const handleDelete = (taskIdToRemove) => {
     // Remove task from task list array
-    setTaskList((prev) => prev.filter(
-      (task) => task.id !== taskIdToRemove
-    ));
+    setTaskList((prev) => prev.filter((task) => task.id !== taskIdToRemove));
   };
 
   const checkTask = (taskIdToCheck, event) => {
     // Toggle task object done property (true or false)
     const toggleDone = (status) => {
-      setTaskList((prev) => prev.map(task => {
-        if (task.id === taskIdToCheck) {
-          return {...task, done: status};
-        }
-        return task;
-      }));
-    }
+      setTaskList((prev) =>
+        prev.map((task) => {
+          if (task.id === taskIdToCheck) {
+            return { ...task, done: status };
+          }
+          return task;
+        })
+      );
+    };
     if (event.target.checked) {
       // If task is checked off...
       toggleDone(true);
-      console.log('✅ Checkbox is checked');
+      console.log("✅ Checkbox is checked");
     } else {
       // If task is unchecked...
       toggleDone(false);
-      console.log('⛔️ Checkbox is NOT checked');
+      console.log("⛔️ Checkbox is NOT checked");
     }
-  }
-  
+  };
+
   return (
     <div>
-        <Header />
+      <Header />
       <main>
-        <NewTask 
+        <NewTask
           newTask={newTask}
           handleChange={handleChange}
           handleSubmit={handleSubmit}
         />
         <DndProvider backend={HTML5Backend}>
-          <TaskList 
+          <TaskList
             taskList={taskList}
+            setTaskList={setTaskList}
             handleDelete={handleDelete}
             handleChange={checkTask}
-          />         
+          />
         </DndProvider>
       </main>
     </div>
--- src/TaskList.js
+++ src/TaskList.js
@@ -1,32 +1,76 @@
-import React, { useRef } from "react";
+import { useRef, useCallback } from 'react';
+import update from "immutability-helper";
 // import useDrag and useDrop hooks from react-dnd
 import { useDrag, useDrop } from "react-dnd";
 
-function Task({title, id, handleDelete, handleChange}){
+function Task({title, id, index, handleDelete, handleChange, moveTask}){
+    const ref = useRef(null);
+
+    // Drop functionality
+    const [{isOver, handlerId}, drop] = useDrop(() => ({
+        accept: "TASK",
+        drop: (item) => moveTask(item.index),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(), // Returns 
+            handlerId: monitor.getHandlerId()
+        }),
+        hover(item, monitor) {
+            if (!ref.current) {
+              return
+            }
+            const dragIndex = item.index
+            const hoverIndex = index
+            // Don't replace items with themselves
+            if (dragIndex === hoverIndex) {
+              return
+            }
+            // Determine rectangle on screen
+            const hoverBoundingRect = ref.current?.getBoundingClientRect()
+            // Get vertical middle
+            const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+            // Determine mouse position
+            const clientOffset = monitor.getClientOffset()
+            // Get pixels to the top
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top
+            // Only perform the move when the mouse has crossed half of the items height
+            // When dragging downwards, only move when the cursor is below 50%
+            // When dragging upwards, only move when the cursor is above 50%
+            // Dragging downwards
+            if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+              return
+            }
+            // Dragging upwards
+            if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+              return
+            }
+            // Time to actually perform the action
+            moveTask(dragIndex, hoverIndex)
+            // Note: we're mutating the monitor item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
+            item.index = hoverIndex
+          },
+    }));
+
+    /*
+    const moveTask = (index) => {
+        console.log(index);
+    };
+    */
+
     // Drag functionality
     const [{isDragging}, drag] = useDrag(() => ({
         type: "TASK",
-        item: { index: 1 },
+        item: { index: 5 },
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging() // Returns boolean 
         })
     }));
 
-    // Drop functionality
-    const [{isOver}, drop] = useDrop(() => ({
-        accept: "TASK",
-        drop: (item) => moveTask(item.index),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver() // Returns boolean
-        })
-    }));
-
-    const moveTask = (index) => {
-        console.log(index);
-    };
-
+    drag(drop(ref));
     return(
-        <div className="task" style={{cursor: isDragging ? "grabbing" : "grab" }} ref={drag} key={id}>
+        <div className="task" style={{cursor: isDragging ? "grabbing" : "grab", opacity: isDragging ? "0" : "1", border: isOver ? "solid #705df2" : "none"}} ref={ref} data-handler-id={handlerId} key={id}>
             <input type="checkbox" onChange={(event) => handleChange(id, event)}/>
             <p>{title}</p>
             <button className="button" type="button" onClick={() => handleDelete(id)}>X</button>
@@ -35,16 +79,31 @@ function Task({title, id, handleDelete, handleChange}){
     );
 }
 
-export default function TaskList({taskList, handleDelete, handleChange}){
+export default function TaskList({taskList, setTaskList, handleDelete, handleChange}){
+
+    const moveTask = useCallback((dragIndex, hoverIndex) => {
+        setTaskList((prev) =>
+          update(prev, {
+            $splice: [
+              [dragIndex, 1],
+              [hoverIndex, 0, prev[dragIndex]]
+            ]
+          })
+        );
+    }, []);
 
     return(
         <div className="task-list">
-                {taskList.map(({title, id}) => (
+                {taskList.map(({title, id, index}) => (
+
                     <Task
                         title={title}
                         id={id}
+                        key={id}
+                        index={index}
                         handleDelete={handleDelete}
                         handleChange={handleChange}
+                        moveTask={moveTask}
                     />
                 ))}
         </div>
