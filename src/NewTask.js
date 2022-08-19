d49be439584cd94d5f6a363cd39c6ee18db0a8b6
Maxime Boucher <maxime@maximeboucher.com>
2022-08-19 17:26:33 -0600

    fix: Adjust submit button icon stroke width

--- src/NewTask.js
+++ src/NewTask.js
@@ -13,7 +13,7 @@ export default function NewTask({newTask, handleChange, handleSubmit}){
                     onChange={handleChange}
                 />
                 <button className="button" type="submit">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.5" width="20px">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.75" width="20px">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
                     </svg>
                 </button>
