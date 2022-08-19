39c7c6ae033205940c8a61c1ad7749a8fd91e5ee
Maxime Boucher <maxime@maximeboucher.com>
2022-08-19 11:31:08 -0600

    fix: Make svg attributes camel case

--- src/Logo.js
+++ src/Logo.js
@@ -1,8 +1,8 @@
 export default function Logo() {
     return(
         <h1 className="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#705df2" width="30px">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#705df2" width="30px">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
             </svg>
             Simple To-Do
         </h1>
